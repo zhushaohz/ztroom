ff904f7d32729d42787fea69320d942723d20178
zhushaohz <zhushaohz@163.com>
2020-04-06 11:59:51 +0800

    登录注册页面

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -15,35 +15,16 @@
         <a class="nav-link" href="#">教程</a>
       </li>
     </ul>
-    <el-row type="flex" justify="space-around">
-        <el-col :span="4"><mcard text="Room1"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room2"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room3"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room4"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room5"></mcard></el-col>
-    </el-row>
-    <div></div>
-    <el-row type="flex" justify="space-around">
-        <el-col :span="4"><mcard text="Room6"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room7"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room8"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room9"></mcard></el-col>
-        <el-col :span="4"><mcard text="Room10"></mcard></el-col>
-    </el-row>
   </div>
 </template>
 
 <script>
-import mcard from './Card'
 export default {
   name: 'School',
   data () {
     return {
-      msg: '选择一个纸条Room'
+      msg: '纸条Room 注册'
     }
-  },
-  components: {
-    mcard
   }
 }
 </script>
@@ -67,8 +48,5 @@ h1 {
     width: 100%;
     --underline-color: #cdcdcd;
     --active-link-color: #000;
-}
-.el-row{
-    margin-top: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import School from '@/components/School'
+import Signup from '@/components/Signup'
+import Login from '@/components/Login'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +17,16 @@ export default new Router({
       path: '/school',
       name: 'School',
       component: School
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
